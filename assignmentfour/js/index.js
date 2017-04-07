6320f4c083ab82131f2e7a954add7f5eef3bb9e4
erick <erickvogel@me.com>
2017-04-07 15:21:01 -0700

    Add files via upload

---
+++ assignmentfour/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".hide").hide (function(){
+        $(".hide").hide();
+    });
+});
+
+$(document).ready(function(){
+    $(".read-more").click(function(){
+        $("#show-this-on-click").show();
+    });
+});
