d2bfb808d10efc5ef3e01a59443c228eed1d3b09
erick <erickvogel@me.com>
2017-04-09 23:25:30 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+    $("a").click(function(event){
+        event.preventDefault();
+    });
+});
+
+$(document).ready(function(){
+    $(".hide").hide();
+});
+
+$(".read-more").click(function(){
+     $("#show-this-on-click").slideDown();
+     $(".read-more").slideUp();
+     $(".read-less").slideDown();
+});
+
+$(".read-less").click(function(){
+     $(".hide").slideUp();
+     $(".read-less").slideUp();
+     $(".read-more").slideDown();
+});
+
+$(".learn-more").click(function(){
+    $(".hide").hide();
+     $("#learn-more-text").slideDown();
+});
